6e9cc76fba77a970371b1c199ce6d2c8bcae5fbe
Sorokin Victor <st-agrail@yandex.ru>
2014-06-19 18:32:08 +0400

    Update add-multiple.js

--- resources/js/add-multiple.js
+++ resources/js/add-multiple.js
@@ -2,6 +2,12 @@ $(document).on("click", ".delete-multi", function () {
     $(this).parent().remove();
     return false;
 });
+$(function () {
+    if ($("ul.multi-ul li").length===0) {
+        var id = $("ul.multi-ul").parent().attr("id");
+        addMulti(id);
+    }
+});
 function addMulti(multi_relation_id) {
     var url = $("#" + multi_relation_id + " input.multi_url").val();
     $.ajax({
